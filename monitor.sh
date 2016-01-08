61a41f57701c7a5ac8a7965c1e368e35384d30bd
zestuart <zstuart@marinsoftware.com>
2016-01-08 23:00:54 +0000

    Bug and silliness fixing
    Didn’t check connectivity to remote host, needed uniformity for
    logging.

--- monitor.sh
+++ monitor.sh
@@ -2,12 +2,12 @@
 
 function environmentTest {
   if [ -f /home/pi/custodes/values/configured ] ; then
-    requiredFiles=(defaultGateway localHostList mtuSize publicIP remoteHost)
+  requiredFiles=(defaultGateway localHostList mtuSize publicIP remoteHost)
   for i in ${requiredFiles[@]} ; do
     if [[ ! -f /home/pi/custodes/values/$i ]] ; then
-      logger "Custodes: event=missing_config_file=$i"
-      cleanRun=1
-      exit 1
+        logger "Custodes: event=missing_config_file=$i"
+        cleanRun=1
+        exit 1
     fi
   done
   else
@@ -28,6 +28,7 @@ function importValues {
 }
 
 function mtuCalc() {
+  #add in logic to only run sometimes; time serial divided by something
   mtuSize=$1
   remoteIP=$2
   mode=0
@@ -41,23 +42,23 @@ function mtuCalc() {
       else
         mtuSize=`expr $mtuSize - 1`
       fi
-      else
-        if [ $pass -eq 0 ] ; then
-          if [ $mtuSize -ne $1 ] ; then
+    else
+      if [ $pass -eq 0 ] ; then
+        if [ $mtuSize -ne $1 ] ; then
             mtuSize=`expr $mtuSize + 9`
             mode=1
             pass=1
           else
-            failPoint=0
-          fi
-          else
-            if [[ $mtuSize -ne $(cat /home/pi/custodes/values/mtuSize) ]] ; then
-              logger "Custodes: event=mtuSizeChanged  oldMTU=$1 newMTU=$mtuSize"
-              cleanRun=1
-              echo $mtuSize > /home/pi/custodes/values/mtuSize
-            fi
-        failPoint=0
+	    failPoint=0
+        fi
+      else
+      if [[ $mtuSize -ne $(cat /home/pi/custodes/values/mtuSize) ]] ; then
+        logger "Custodes: event=mtuSizeChanged  oldMTU=$1 newMTU=$mtuSize"
+        cleanRun=1
+        echo $mtuSize > /home/pi/custodes/values/mtuSize
       fi
+    failPoint=0
+    fi
     fi
   done
 }
@@ -65,15 +66,15 @@ function mtuCalc() {
 function hostPoll() {
   ping -c 1 -t 5 $1 &> /dev/null
   if [ $? -eq 0 ]; then
-    return 0
-  else
-    return 1
+      return 0
+    else
+      return 1
   fi
 }
 
 function localSitePoll {
   for i in ${localHostList[@]} ; do
-      hostPoll $i
+    hostPoll $i
     if [ $? -eq 1 ]; then
       logger "Custodes: event=localHostDown ip=$i"
       cleanRun=1
@@ -81,12 +82,21 @@ function localSitePoll {
   done
 }
 
+function internetCheck() {
+  hostPoll $1
+  if [ $? -eq 1 ]; then
+	logger "Custodes: event=internetDown"
+	clearnrun=1
+  fi
+}
+
 function publicIPcheck() {
   currentPublicIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
   if [ $? -eq 10 ] ; then 
     logger "Custodes: event=unable_to_determine_public_ip"
+    clearnrun=1
     return 1
-  else
+    else
     if [[ $1 != $currentPublicIP ]] ; then
       logger "Custodes: event=publicIPchanged old_public_ip=$1 new_public_ip=$currentPublicIP"
       echo $currentPublicIP > /home/pi/custodes/values/publicIP
@@ -102,6 +112,7 @@ hostPoll $defaultGateway
 if [ $? -eq 0 ] ; then
   publicIPcheck $publicIP  
   localSitePoll
+  internetCheck $remoteHost
   mtuCalc $mtuSize $remoteHost
 else
   echo "Custodes: event=default_gateway_no_icmp"
